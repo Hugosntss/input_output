6cfda4e448a8af9a047ef5f0dd96d098bb6984c6
[Hugo] <[hugosnts.sousa@gmail.com]>
2024-09-07 10:41:37 -0700

    comments added

--- input.cc
+++ input.cc
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main (){
-    cout<< "enter 3 numbers 1-10:"; 
+    cout<< "enter 3 numbers 1-10:"; // This is the output asking 3 numbers to the user(collecting data)
 
     int a,b,c;
-    cin>>a>>b>>c; 
+    cin>>a>>b>>c; // This is the input for the user, A;B;C are 3 integer numbers which are going to be offered by the user
 
-    cout<<"your numbers are: " <<a<<";"<<b<<";"<<c<<endl;
+    cout<<"your numbers are: " <<a<<";"<<b<<";"<<c<<endl; 
   return 0;
 }
