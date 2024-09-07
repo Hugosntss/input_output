124e39b13bfd6a3dcd7c93d057e6a27bd1a4032e
[Hugo] <[hugosnts.sousa@gmail.com]>
2024-09-07 10:34:11 -0700

    input/output program

---
+++ input.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout<< "enter 3 numbers 1-10:"; 
+
+    int a,b,c;
+    cin>>a>>b>>c; 
+
+    cout<<"your numbers are: " <<a<<";"<<b<<";"<<c<<endl;
+  return 0;
+}
